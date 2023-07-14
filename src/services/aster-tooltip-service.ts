de85b6254d72908eea2f63602c7e2a0cad8bfafa
Sébastien Doucet <sebdoucet@gmail.com>
2023-07-14 10:49:57 -0400

    Improve tooltip align

--- src/services/aster-tooltip-service.ts
+++ src/services/aster-tooltip-service.ts
@@ -1,16 +1,37 @@
 import { ServiceContract } from "@aster-js/ioc";
-import { Tooltip } from "@aster-elements/tooltip";
-import { ITooltipService } from "../abstraction";
+import { Tooltip, TooltipAlign, TooltipPosition } from "@aster-elements/tooltip";
+import { ITooltipService, TimelineRenderingOptions } from "../abstraction";
 
 @ServiceContract(ITooltipService)
-export class AsterTooltipService implements ITooltipService {
+export class AsterTooltipService<T> implements ITooltipService<T> {
 
     constructor(
         private readonly _tooltipName?: string
     ) { }
 
-    show(ev: UIEvent, content: unknown): void {
-        const tooltip = Tooltip.get(this._tooltipName);
-        tooltip.show(ev, content, { autoHide: true, align: "center", position: "bottom" });
+    show(ev: UIEvent, content: unknown, item: T, index: number, items: T[], opts: TimelineRenderingOptions): void {
+
+        const position = this.resolveTooltipPosition(index, opts);
+        const align = this.resolveTooltipAlign(index, items, opts);
+
+        this.getTooltip().show(ev, content, { autoHide: true, align, position });
+    }
+
+    protected getTooltip(): Tooltip {
+        return Tooltip.get(this._tooltipName)
+    }
+
+    private resolveTooltipAlign(index: number, items: T[], opts: TimelineRenderingOptions): TooltipAlign {
+        if (index === (items.length - 1)) {
+            return "end";
+        }
+        return opts.layout === "horizontal" ? "center" : "start";
+    }
+
+    private resolveTooltipPosition(index: number, opts: TimelineRenderingOptions): TooltipPosition {
+        if ((index % 2) == 0) {
+            return opts.layout === "horizontal" ? "bottom" : "right";
+        }
+        return opts.layout === "horizontal" ? "top" : "left";
     }
 }
